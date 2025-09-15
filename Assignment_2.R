7746785cd650a24adfed8f0e39b78d2ed52c4635
mcmrtn <amcmarteen@gmail.com>
2025-09-15 06:24:05 -0700

    first commit

---
+++ Assignment_2.R
@@ -0,0 +1,14 @@
+sample_Input <- 72
+
+if(sample_Input >= 90 && sample_Input <= 100){
+  print("Grade: A")
+}else if(sample_Input >= 80 && sample_Input <= 89){
+  print("Grade: B")
+}else if(sample_Input >= 70 && sample_Input <= 79){
+  print("Grade: C")
+}else if(sample_Input >= 60 && sample_Input <= 69){
+  print("Grade: D")
+}else{
+  print("Grade:F")
+}
+
---
+++ Assignment_3.R
@@ -0,0 +1,10 @@
+
+temperature <- 25
+
+if (temperature >= 30){
+  print("Hot")
+}else if (temperature >= 20 && temperature <= 29){
+  print("Warm")
+}else if (temperature < 20){
+  print("Cold")
+}
